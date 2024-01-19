c68d112b78f8f40e632574955653aaa2b7b529cc
Abdul Moiz Khan <abdulmoizkhan140@gmail.com>
2024-01-20 02:40:44 +0500

    folder structure

---
+++ pages/services/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+import { Services } from '../../views'
+
+const Service = () => {
+  return (
+    <Layout>
+      <Services/>
+    </Layout>
+  )
+}
+
+export default Service
---
+++ views/index.ts
@@ -0,0 +1,7 @@
+import HomePage from "./home"
+import About from "./about"
+import Services from "./services"
+import ContactUs from "./contact-us"
+
+export {HomePage,About,Services,ContactUs}
+
