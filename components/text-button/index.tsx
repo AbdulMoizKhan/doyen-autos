1119dda1b7561df2a44dc3c66c08d4aa89b775b9
dani2202 <daniyalq2202@gmail.com>
2024-01-20 18:55:43 +0500

    TextButton component created

---
+++ components/text-button/index.tsx
@@ -0,0 +1,39 @@
+import { Typography } from "@mui/material";
+import { useRouter } from 'next/navigation'
+
+interface Props {
+  title: string;
+  color: string;
+  hoverColor: string;
+  redirectLink: string;
+}
+
+const TextButton = ({
+  title,
+  color,
+  hoverColor,
+  redirectLink,
+}: Props): JSX.Element => {
+  const router = useRouter();
+  return (
+    <Typography
+      sx={{
+        color: color,
+        fontSize: "16px",
+        fontWeight: 500,
+        cursor: "pointer",
+        transition: "0.3s",
+        "&:hover": {
+          color: hoverColor,
+        },
+      }}
+      onClick={() => {
+        router.push(redirectLink);
+      }}
+    >
+      {title}
+    </Typography>
+  );
+};
+
+export default TextButton;
