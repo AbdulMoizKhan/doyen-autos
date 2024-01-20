d6a0abf700d9db5ceb588d9b193016aaee3582c1
dani2202 <daniyalq2202@gmail.com>
2024-01-20 18:56:07 +0500

    pages created

---
+++ pages/about/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { About } from "../../views";
+import Header from "../../components/header";
+
+const AboutUs = () => (
+  <>
+    <Header />
+    <About />
+  </>
+);
+
+export default AboutUs;
