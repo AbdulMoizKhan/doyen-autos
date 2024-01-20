ec1a3522261fc71a5dec4fc8dda686591e574012
dani2202 <daniyalq2202@gmail.com>
2024-01-20 18:01:59 +0500

    textfield component created

--- views/home/index.tsx
+++ views/home/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import TextField from "../../components/text-field";
+import { Box } from "@mui/material";
 
 const HomePage = () => {
   return (
     <>
       <h1>home</h1>
+      <TextField name="test" placeholder="test" />
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
