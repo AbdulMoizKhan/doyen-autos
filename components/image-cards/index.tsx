9cb95cf704d31946e86d4b05c2023b0a815df316
Abdul Moiz Khan <abdulmoizkhan140@gmail.com>
2024-01-21 17:17:12 +0500

    component added

---
+++ components/image-cards/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button, Container, Grid, Stack, Typography } from '@mui/material';
+
+const ImageCards = () => {
+    return (
+        <div>
+            <Container>
+                <Stack direction="column" spacing={2}>
+                <Typography variant="h1" gutterBottom={true}>
+                    Garages we trust and you love
+                </Typography>
+                </Stack>
+            </Container>
+        </div>
+    )
+}
+
+export default ImageCards
--- views/home/index.tsx
+++ views/home/index.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import HeroSection from "../../components/hero-section";
+import { Divider } from "@mui/material";
+import ImageText from "../../components/image-text";
+import ImageCards from "../../components/image-cards";
 
 const HomePage = () => {
   const changedText = ['Repair', 'MOT', 'Car Service'];
@@ -16,10 +19,14 @@ const HomePage = () => {
   return (
     <>
       <HeroSection
-        heroTittle={`Compare Instant </br> ${changedText[currentTitleIndex]} </br> Deals.`}
-        heroSubContent="Join the 13,240,704 drivers who have compared local prices to book, so you can get on with the things that make you smile."
+        heroTittle={`Compare Instant </br> ${changedText[currentTitleIndex]} </br> Deals`}
+        heroSubContent="Join the 13,240,704 drivers who have compared local prices to book, so you can get on with the things that make you smile"
         buttonText="Get Started"
       />
+      <Divider sx={{borderColor:'#fff',margin:'40px 0px'}}></Divider>
+      <ImageText/>
+      <Divider sx={{borderColor:'#fff',margin:'40px 0px'}}></Divider>
+      <ImageCards/>
     </>
   );
 };
