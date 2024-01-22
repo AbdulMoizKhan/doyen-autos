2d4bfbf780facebdcd99f3c69ae96e7d103a04f9
dani2202 <daniyalq2202@gmail.com>
2024-01-23 01:29:28 +0500

    header called

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import { HomePage } from "../views";
+import Footer from "../components/footer";
 import Header from "../components/header";
 
 const IndexPage = () => (
   <>
     <Header />
     <HomePage />
+    <Footer/>
   </>
 );
 
