f82a7ff3d633a6f535e667dc93cf651ec2d698b9
Dani2202 <daniyalq2202@gmail.com>
2024-01-24 14:10:49 +0500

    helpers added for servcies

--- utils/helpers.ts
+++ utils/helpers.ts
@@ -4,3 +4,24 @@ export const TextLinks = [
   { title: "Services", url: "/services" },
   { title: "Contact Us", url: "/contact-us" },
 ];
+
+
+export const serviceOptions = [
+  "Mechanical Repair",
+  "Electrical Repair",
+  "Interim service",
+  "Mot",
+  "Full service",
+  "Oil Change (Incl. Filters)",
+  "Remapping /Tuning",
+  "Key cutting",
+  "Immobilizer",
+  "ECU Repair",
+  "AdBlue Delete",
+  "EGR Delete",
+  "DPF Delete",
+  "DTCs Delete",
+  "Speed Limit Removal",
+  "Mileage Correction ",
+  "Others"
+];
