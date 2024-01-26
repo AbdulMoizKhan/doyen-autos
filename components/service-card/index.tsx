39c3807eb3f244e43da8513ed52d4482686e6111
Dani2202 <daniyalq2202@gmail.com>
2024-01-26 13:46:32 +0500

    WIP

---
+++ components/service-card/index.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
+interface IServiceCard {
+  heading: string;
+  imgSrc: string;
+  body: string;
+}
+
+export default function ServiceCard({ heading, imgSrc, body }: IServiceCard) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image={imgSrc}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {heading}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {body}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
--- views/services/index.tsx
+++ views/services/index.tsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import { Box, Divider, Grid, Typography } from "@mui/material";
+import React from "react";
+import ServiceCard from "../../components/service-card";
 
 const Services = () => {
   return (
-    <>
-    </>
-  )
-}
+    <Box sx={{ pt: "95px" }}>
+      <Box component="div" sx={{ m: { xs: 2, sm: 8 } }}>
+        <Typography
+          variant="h1"
+          textAlign="center"
+          sx={{ mt: 4 }}
+          data-aos="zoom-out"
+        >
+          Our Auto Services
+        </Typography>
+        <Typography
+          variant="h6"
+          textAlign="center"
+          sx={{ mt: 2, mb: { xs: 5, sm: 10 } }}
+          data-aos="fade-right"
+        >
+          At Doyen Auto Services, we take pride in offering a comprehensive
+          range of automotive services to keep your vehicle running smoothly.
+          Our skilled team of mechanics is dedicated to providing high-quality
+          workmanship and exceptional customer service. Whether you drive a
+          domestic or foreign vehicle, we have the expertise to meet all your
+          automotive needs. Explore our services below:
+        </Typography>
+        <Divider
+          sx={{
+            backgroundColor: "black",
+            height: "1px",
+            mb: { xs: 5, sm: 10 },
+          }}
+        />
+      </Box>
+      <Grid container  margin={3}>
+        <Grid item xs={4}>
+          <ServiceCard
+            heading={"Routine Maintenance"}
+            body={`ABC`}
+            imgSrc=""
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <ServiceCard
+            heading={"Routine Maintenance"}
+            body={`ABC`}
+            imgSrc=""
+          />
+        </Grid>
+        <Grid item xs={4}>
+          <ServiceCard
+            heading={"Routine Maintenance"}
+            body={`ABC`}
+            imgSrc=""
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
-export default Services
+export default Services;
